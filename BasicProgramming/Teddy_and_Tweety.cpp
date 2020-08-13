b3adc78896737f5fcdf522030492060675ee26ba
Mohnish Vegi <vegimohnish@gmail.com>
2020-08-13 20:11:46 +0530

    Create Teddy_and_Tweety.cpp

---
+++ BasicProgramming/Teddy_and_Tweety.cpp
@@ -0,0 +1,14 @@
+//VEGI MOHNISH - Indian Institute of Information Technology Design and Manufacturing(IIITDM) Kancheepuram
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	if(n%3 == 0)
+		cout << "YES";
+	else 
+		cout << "NO";
+	return 0;
+}
