567d85b037e1b78fcf492cf582ab00e762cc586b
Mohnish Vegi <vegimohnish@gmail.com>
2020-08-13 20:46:40 +0530

    Create I_am_easy.cpp

---
+++ BasicProgramming/I_am_easy.cpp
@@ -0,0 +1,14 @@
+//VEGI MOHNISH - Indian Institute of Information Technology Design and Manufacturing(IIITDM) Kancheepuram
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n,p;
+	cin >> n;
+	for(int i=1;i<=10;i++) {
+		p = n*i;
+		cout << p << endl;
+	}
+	return 0;
+}
