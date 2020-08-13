9643f2777d03dba0a73366b9f53d39ecadf1f0dd
Mohnish Vegi <vegimohnish@gmail.com>
2020-08-13 16:18:27 +0530

    Create Print_The_Numbers.cpp

---
+++ BasicProgramming/Print_The_Numbers.cpp
@@ -0,0 +1,19 @@
+//VEGI MOHNISH - Indian Institute of Information Technology Design and Manufacturing(IIITDM) Kancheepuram
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int a[n];
+	for(int i=0;i<n;i++) {
+		cin >> a[i];
+		cout << a[i] << " ";
+	}
+	
+
+	
+	return 0;
+}
