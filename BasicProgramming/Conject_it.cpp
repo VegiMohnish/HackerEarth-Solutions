ba1dcbe5c5b477f0925a22eaf17c91d152146e11
Mohnish Vegi <vegimohnish@gmail.com>
2020-08-14 19:25:07 +0530

    Create Conject_it.cpp

---
+++ BasicProgramming/Conject_it.cpp
@@ -0,0 +1,15 @@
+//VEGI MOHNISH - Indian Institute of Information Technology Design and Manufacturing(IIITDM) Kancheepuram 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int t; 
+  cin>>t; 
+  while(t--) { 
+	  int x; 
+	  cin>>x; 
+	  cout<<"YES"<<endl; 
+	  }
+  return 0;
+}
