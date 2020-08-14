c66ace6616afbb7f71458ded91ceef344e5b3565
Mohnish Vegi <vegimohnish@gmail.com>
2020-08-14 11:12:22 +0530

    Create Hello.cpp

---
+++ BasicProgramming/Hello.cpp
@@ -0,0 +1,9 @@
+//VEGI MOHNISH - Indian Institute of Information Technology Design and Manufacturing(IIITDM) Kancheepuram
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()  {
+	cout << "Hello Kirti";
+	return 0;
+}
