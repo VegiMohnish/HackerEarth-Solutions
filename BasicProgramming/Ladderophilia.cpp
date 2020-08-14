58d2f293986e931d128a3fd201fb747bc86859ad
Mohnish Vegi <vegimohnish@gmail.com>
2020-08-14 18:28:11 +0530

    Create Ladderophilia.cpp

---
+++ BasicProgramming/Ladderophilia.cpp
@@ -0,0 +1,20 @@
+//VEGI MOHNISH - Indian Institute of Information Technology Design and Manufacturing(IIITDM) Kancheepuram 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int i=1;
+	while(i<=n) {
+		cout << '*' << "   " << '*' << endl;
+		cout << '*' << "   " << '*' << endl;
+		cout << "*****" << endl;
+		i++;
+
+	}
+		cout << '*' << "   " << '*' << endl;
+		cout << '*' << "   " << '*' << endl;
+		return 0;
+}
