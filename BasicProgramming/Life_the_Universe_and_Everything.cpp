affb3ddf200be8d978a7c848a83b1832961460a3
Mohnish Vegi <vegimohnish@gmail.com>
2020-08-14 11:09:36 +0530

    Create Life_the_Universe_and_Everything.cpp

---
+++ BasicProgramming/Life_the_Universe_and_Everything.cpp
@@ -0,0 +1,16 @@
+//VEGI MOHNISH - Indian Institute of Information Technology Design and Manufacturing(IIITDM) Kancheepuram
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	while(1) {
+		cin >> n;
+		if(n == 42) 
+			break;
+		else 
+			cout << n << endl;
+	}
+	return 0;
+}
