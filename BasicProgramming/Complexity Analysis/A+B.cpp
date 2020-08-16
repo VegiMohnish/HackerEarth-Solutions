1ce00473d4a3d8e7e121a7e32ac088dcd2359982
Mohnish Vegi <vegimohnish@gmail.com>
2020-08-16 13:24:50 +0530

    Create A+B.cpp

---
+++ BasicProgramming/Complexity Analysis/A+B.cpp
@@ -0,0 +1,13 @@
+//VEGI MOHNISH - Indian Institute of Information Technology Design and Manufacturing(IIITDM) Kancheepuram 
+
+#include<bits/stdc++.h>
+#include<boost/multiprecision/cpp_int.hpp>
+using namespace std;
+using namespace boost::multiprecision;
+int main(){
+    cpp_int a,b;
+    while(cin>>a && cin>>b){
+        cout<<a+b<<endl;
+    }
+    return 0;
+}
