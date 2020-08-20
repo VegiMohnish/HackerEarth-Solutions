4d59ef03cd9a796d752deee36a3d6861a2086b3d
Mohnish Vegi <vegimohnish@gmail.com>
2020-08-20 12:49:27 +0530

    Create Fitting_Circles.cpp

---
+++ BasicProgramming/Implementation/Fitting_Circles.cpp
@@ -0,0 +1,18 @@
+//VEGI MOHNISH - Indian Institute of Information Technology Design and Manufacturing(IIITDM) Kancheepuram 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		long long a,b;
+		cin >> a >> b;
+		if(a>b)
+		cout << a/b << endl;
+		else 
+		cout << b/a << endl;
+	}
+	return 0;
+}
